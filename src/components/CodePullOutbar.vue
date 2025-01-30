3a26849bda4a65ddd62754ab46b2767b971f17da
xf <14182909+xh_13_0@user.noreply.gitee.com>
2025-01-30 14:57:31 +0800

    拖拽组件draggable的添加和使用

--- src/components/CodePullOutbar.vue
+++ src/components/CodePullOutbar.vue
@@ -7,24 +7,28 @@ const itemList = ref([
 
 <template>
   <!-- 测拉栏 -->
-  <div class="w-[60px] bg-slate-200 shadow-2xl fixed h-[100vh] z-10 overflow-hidden pullOutbar">
-    <div v-for="(item, index) in itemList" :key="index" class="w-[800px] h-[30px] ">
+  <div class=" bg-slate-200 shadow-2xl fixed h-[100vh] z-10 overflow-hidden pullOutbar opacity-90">
+    <section v-for="(item, index) in itemList" :key="index" class="w-[800px] h-[30px]">
       <img :src="item.icon" alt="" class=" relative size-[40px] top-[200px] mt-[100px] left-[11px] opacity-[50%] icon">
-      <RouterLink :to="item.path" class="text-[30px] relative left-[100px] flex top-[160px] ">
+      <RouterLink :to="item.path" class="text-[30px] relative left-[100px] flex top-[160px]">
         {{ item.title }}
       </RouterLink>
-    </div>
+    </section>
   </div>
 </template>
 
 <style scoped>
- div > div:hover {
-  color: rgb(134, 134, 206);
-  width: 300px;
+div > div {
+  color: inherit; /* 恢复默认颜色 */
+  width: 70px; /* 恢复默认宽度 */
   transition:
     color 0.9s ease,
     width 0.5s ease,
     transform 0.9s ease;
+}
+
+div > div:hover {
+  width: 250px;
 }
 .dark .pullOutbar {
   color: aliceblue;
--- src/page/MyCollection.vue
+++ src/page/MyCollection.vue
@@ -1,16 +1,38 @@
 <script lang="ts" setup>
-const connection = [
+import { ref } from 'vue'
+import { vDraggable } from 'vue-draggable-plus'
+
+const connection = ref([
   { title: 'node', img: '/src/assets/vscode-icons--file-type-node.png', url: 'https://nodejs.org/zh-tw' },
   { title: 'element', img: '/src/assets/ep--element-plus.png', url: 'https://element-plus.org/zh-CN/' },
   { title: 'vue3', img: '/src/assets/Vue.png', url: 'https://cn.vuejs.org/' },
-]
+  { title: 'nuxt.js', img: '/src/assets/devicon--nuxtjs.png', url: 'https://www.nuxtjs.cn/' },
+])
+
+// 从localStorage中读取保存的状态即重新调用该状态
+const savedState = localStorage.getItem('draggableState')
+if (savedState) {
+  connection.value = JSON.parse(savedState)
+}
+
+// 在拖拽结束后通过setItem来保存状态
+function saveState() {
+  localStorage.setItem('draggableState', JSON.stringify(connection.value))
+}
 </script>
 
 <template>
   <div class="flex absolute top-[140px] text-[38px] animate__animated animate__fadeIn">
     {{ $t('home.myCollection.description') }}
   </div>
-  <div class=" relative top-[-80px] w-[87%] flex shadow-inner bg-inherit flex-wrap">
+  <div
+    v-draggable="[
+      connection,
+      {
+        animation: 150,
+        onEnd: saveState,
+      },
+    ]" class=" relative top-[-80px] w-[87%] flex shadow-inner bg-inherit flex-wrap">
     <a v-for="(item, index) in connection" :key="index" :href="item.url" target="_blank" class=" flex-wrap relative h-[190px] m-[20px] w-[150px] flex justify-center items-center rounded-[30px] box-shadow-custom container">
       <img class="size-25 top-[10px] absolute" :src="item.img">
       <text class="text-[30px] relative top-[70px]">
